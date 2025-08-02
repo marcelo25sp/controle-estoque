066b8491e01ca6ad56a5fd045af5f6955ab1c819
Marcelo Nascimento <marcelo.ngomes@hotmail.com>
2025-08-01 23:24:25 -0300

    implementa a classe Produto com atributos e construtor

--- src/entities/Produto.java
+++ src/entities/Produto.java
@@ -2,4 +2,16 @@ package entities;
 
 public class Produto {
 
+	private int codigo;
+	private String nome;
+	private double preco;
+	private int quantidade;
+
+	public Produto(int codigo, String nome, double preco, int quantidade) {
+		this.codigo = codigo;
+		this.nome = nome;
+		this.preco = preco;
+		this.quantidade = quantidade;
+	}
+
 }
